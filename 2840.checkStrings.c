1de7616bd0320086488ccd89d89d3b35bdf24833
PotatoHuang <stevenhuang.dog@gmail.com>
2026-03-29 10:04:44 +0800

    2026 March 29th daily and 2840(Tomorrow's maybe?)

---
+++ 2840.checkStrings.c
@@ -0,0 +1,42 @@
+#include "shdstd.h"
+
+bool checkStrings(char *s1, char *s2)
+{
+	int size = strlen(s1);
+	int s2_size = strlen(s2);
+
+	if (size != s2_size) {
+		return false;
+	}
+
+	int odd_freq[26] = {0};
+	int even_freq[26] = {0};
+
+	for (int i = 0; i < size; ++i) {
+		int ch = s1[i] - 'a';
+
+		if (i % 2 == 0) {
+			++even_freq[ch];
+		} else {
+			++odd_freq[ch];
+		}
+	}
+
+	for (int i = 0; i < size; ++i) {
+		int ch = s2[i] - 'a';
+
+		if (i % 2 == 0) {
+			--even_freq[ch];
+		} else {
+			--odd_freq[ch];
+		}
+	}
+
+	for (int i = 0; i < 26; ++i) {
+		if (odd_freq[i] != 0 || even_freq[i] != 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
